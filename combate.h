58e5f76e9442d049bf1f772fb01201822563c0cd
Anpacaco <anpacacontente@gmail.com>
2026-06-09 17:05:33 -0300

    função para imprimr os dados do personagem

---
+++ combate.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+void exibir(string nome, int vida, int ataque){
+	cout<<"\_"<<nome<<"_/";
+	cout<<"Vida"<<vida;
+	cout<<"Ataque"<<ataque;
+}
+
