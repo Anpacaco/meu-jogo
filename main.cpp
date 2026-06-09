3d1c379583cc479ce07c10ff07e2b551b00b5cfb
Anpacaco <anpacacontente@gmail.com>
2026-06-09 17:12:21 -0300

    programa que imprime as informações do personagem

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <string>
+#include combate.h
+using namespace std;
+
+exibir(Esqueleto-Magico, 2, 50){
+	cout<<vida
+}
 
 int main(){
-	std::cout<<"two two game";
-	std::cout<<"programa finalizado. Obrigado";
+	cout<<"two two game";
+	cout<<"programa finalizado. Obrigado";
 	return 0;
 }
